a74880a34767492fc69f877f7d2d048ab2e8038e
Ivaylo Apostolov <ivaylo.apostolov@gmail.com>
2019-04-09 09:44:01 +0300

    Create subfolder by Ivaylo to be able to merge

---
+++ QC_Automation_TeamWork/QC_Automation_TeamWork/Core/BasePage.cs
@@ -0,0 +1,13 @@
+﻿namespace QC_Automation_TeamWork.Core
+{
+    public class BasePage<TM> where TM : BasePageElementMap, new()
+    {
+        protected TM Map
+        {
+            get
+            {
+                return new TM();
+            }
+        }
+    }
+}
---
+++ QC_Automation_TeamWork/QC_Automation_TeamWork/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("QC_Automation_TeamWork")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("QC_Automation_TeamWork")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("17391d5c-c12e-4af6-a4e1-818798b173d6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
