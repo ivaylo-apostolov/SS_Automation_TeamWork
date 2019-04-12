0f75791f04860f59b588cac54b119d35add74f8b
Mladen Varbev <mladen_gsi@abv.bg>
2019-04-12 22:07:20 +0300

    Refactoring

--- QC_Automation_TeamWork/QC_Automation_TeamWork/Pages/StoreSearchPage/StoreSearchResultPage.cs
+++
@@ -1,8 +0,0 @@
-﻿using QC_Automation_TeamWork.Core;
-
-namespace QC_Automation_TeamWork.Pages
-{
-    public class StoreSearchResultPage : BasePage<StoreSearchResultPageElementMap, StoreSearchResultPageValidator>
-    {
-    }
-}
