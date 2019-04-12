1317a4246a63f801d63254a48fc0c9c123570e25
madlenaivanova <madlena.ivanova85@gmail.com>
2019-04-12 21:21:06 +0300

    Fixed after merge.

--- QC_Automation_TeamWork/QC_Automation_TeamWork/Pages/OpenCartPinSecurityCheckPage/PinSecurityCheckPageValidator.cs
+++ QC_Automation_TeamWork/QC_Automation_TeamWork/Pages/OpenCartPinSecurityCheckPage/PinSecurityCheckPageValidator.cs
@@ -1,9 +1,5 @@
 ﻿using QC_Automation_TeamWork.Core;
 
-<<<<<<< HEAD
-=======
-
->>>>>>> Madlen_feature
 namespace QC_Automation_TeamWork.Pages
 {
     public class PinSecurityCheckPageValidator : BasePageValidator<PinSecurityCheckPageElementMap>
