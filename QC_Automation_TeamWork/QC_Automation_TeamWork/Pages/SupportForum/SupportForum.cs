aac3b2c78b3eceebf4199ac6a179d6193fca5abf
Mladen Varbev <mladen_gsi@abv.bg>
2019-04-12 21:49:29 +0300

    Refactoring

--- QC_Automation_TeamWork/QC_Automation_TeamWork/Pages/SupportForum/SupportForum.cs
+++
@@ -1,9 +0,0 @@
-﻿using QC_Automation_TeamWork.Core;
-using QC_Automation_TeamWork.Data.Models;
-
-namespace QC_Automation_TeamWork.Pages
-{
-    public class SupportForum : BasePage<SupportForumElementMap, SupportForumValidator>
-    {
-    }
-}
